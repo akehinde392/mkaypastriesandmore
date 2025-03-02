b59fae15dc00d4722016c9767fed85017bd65e5a
akehinde392 <akehinde392@gmail.com>
2025-03-02 12:43:24 -0800

    new update

--- kaffy.js
+++ kaffy.js
@@ -21,3 +21,21 @@ document.querySelectorAll('.add-to-cart').forEach(button => {
         map: map,
     });
 }
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.querySelectorAll(".slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 4000); // Change slide every 3 seconds
+}
